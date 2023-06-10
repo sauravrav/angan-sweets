c73c196a64afafaf8b5f2e65c739ba0e60338aab
sauravb <s.bhatta2383@gmail.com>
2023-06-11 00:04:09 +0545

    Added item on search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,33 @@ import 'react-select-search/style.css'
 import Image from "next/image";
 import SearchComponent from "./searchComponent";
 import { sweets } from "@/data/sweets";
+import { bakery } from "@/data/bakery";
+import { cakes } from "@/data/cakes";
+import { chats } from "@/data/chats";
+import { chinese } from "@/data/chinese";
+import { fasts } from "@/data/fast";
+import { namkeens } from "@/data/namkeen";
+import { northIndian } from "@/data/north";
+import { southIndian } from "@/data/south";
+import { snacks } from "@/data/snacks";
+import { sweetbox } from "@/data/sweetbox";
+import { tawaTandoor } from "@/data/tawa";
+
+const searchItems = [
+    ...sweets.listOfItems,
+    ...bakery.listOfItems,
+    ...namkeens.listOfItems,
+    ...cakes.listOfItems,
+    ...chats.listOfItems,
+    ...chinese.listOfItems,
+    ...fasts.listOfItems,
+    ...namkeens.listOfItems,
+    ...northIndian.listOfItems,
+    ...southIndian.listOfItems,
+    ...snacks.listOfItems,
+    ...sweetbox.listOfItems,
+    ...tawaTandoor.listOfItems
+]
 
 const Navbar = () => {
     return ( 
@@ -13,7 +40,7 @@ const Navbar = () => {
                 <Image src='/img/logo.png' alt='logo' width={100} height={50}/>
             </div>
             <div className="search">
-            <SearchComponent items={sweets.listOfItems} />
+                <SearchComponent items={searchItems} />
             </div>
             <div className="contacts">
                 Tripureshwor : 9802318809 / 01-4212502 <br />
