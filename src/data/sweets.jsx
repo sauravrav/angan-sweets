ebf9d8a23707efa06c8f09775ff88c910a2b19db
sauravb <s.bhatta2383@gmail.com>
2023-06-15 01:08:39 +0545

    Added sweets up to dahi small

--- src/data/sweets.jsx
+++ src/data/sweets.jsx
@@ -110,5 +110,395 @@ export const sweets = {
           priceP: 'Nrs. 75/pc',
           priceK: '--'
         },
+        {
+          imgSrc: `${sweetPath}/dil-khusar.jpg`,
+          title: 'Dil Khusar',
+          priceP: 'Nrs. 70/pc',
+          priceK: 'Nrs. 1850/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/finni.png`,
+          title: 'Finni',
+          priceP: '--',
+          priceK: 'Nrs. 1450/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/gajarr-halwa.jpg`,
+          title: 'Gajar Halwa',
+          priceP: '--',
+          priceK: 'Nrs. 1800/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/moong-dal-barfi.jpg`,
+          title: 'Moong Dal',
+          priceP: 'Nrs. 45/pc',
+          priceK: 'Nrs. 1600/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/doda-barfi.webp`,
+          title: 'Doda Barfi',
+          priceP: 'Nrs. 50/pc',
+          priceK: 'Nrs. 1800/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/mewa bati.jpg`,
+          title: 'Mewa Bati',
+          priceP: 'Nrs. 60/pc',
+          priceK: 'Nrs. 1750/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/kaju-tarbuj.jpg`,
+          title: 'Kaju Tarbuj',
+          priceP: 'Nrs. 70/pc',
+          priceK: 'Nrs. 2400/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/Pakeeza.jpg`,
+          title: 'Pakeeza',
+          priceP: 'Nrs. 75/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/moti-pak.jpg`,
+          title: 'Moti Pak',
+          priceP: 'Nrs. 50/pc',
+          priceK: 'Nrs. 1750/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/sada-peda.jpg`,
+          title: 'Sada Peda',
+          priceP: 'Nrs. 50/pc',
+          priceK: 'Nrs. 1750/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/kaju-roll.jpg`,
+          title: 'Kaju Roll',
+          priceP: 'Nrs. 60/pc',
+          priceK: 'Nrs. 2250/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/khoya-pista.jpg`,
+          title: 'Khoya Pista',
+          priceP: 'Nrs. 50/pc',
+          priceK: 'Nrs. 1750/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/parwal.jpg`,
+          title: 'Parwal',
+          priceP: 'Nrs. 85/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/badam-pista-bahar.jpg`,
+          title: 'Badam Pista Bahar',
+          priceP: 'Nrs. 75/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/choco_barfi.jpg`,
+          title: 'Chocolate Barfi',
+          priceP: 'Nrs. 50/pc',
+          priceK: 'Nrs. 1800/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/kaju katli.jpg`,
+          title: 'Kaju Katli',
+          priceP: 'Nrs. 50/pc',
+          priceK: 'Nrs. 1950/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/badam phool.jpg`,
+          title: 'Badam Phool',
+          priceP: 'Nrs. 55/pc',
+          priceK: 'Nrs. 2150/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/kaju gujiya.jpg`,
+          title: 'Kaju Gujiya',
+          priceP: 'Nrs. 55/pc',
+          priceK: 'Nrs. 2150/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/kaju-pista-dilkhus.jpeg`,
+          title: 'Kaju Pista Dilkhush',
+          priceP: 'Nrs. 65/pc',
+          priceK: 'Nrs. 2800/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/badam pista tirangi.jpg`,
+          title: 'Badam Pista Tirangi',
+          priceP: 'Nrs. 65/pc',
+          priceK: 'Nrs. 2800/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/badam-pista-roll.jpeg`,
+          title: 'Badam Pista Roll',
+          priceP: 'Nrs. 65/pc',
+          priceK: 'Nrs. 2800/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/chocolate-sohan-papdi.jpg`,
+          title: '250GM CHOCOLATE SOAN PAPDI',
+          priceP: 'Nrs. 395/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/kesar bati.jpg`,
+          title: 'Kesar Bati',
+          priceP: 'Nrs. 70/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/khajur barfi.jpg`,
+          title: 'Khajur Barfi',
+          priceP: 'Nrs. 55/pc',
+          priceK: 'Nrs. 1850/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/khoya-cake.jpeg`,
+          title: 'Khoya Cake',
+          priceP: 'Nrs. 45/pc',
+          priceK: 'Nrs. 1450/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/khoya petha.jpeg`,
+          title: 'Khoya Petha',
+          priceP: 'Nrs. 45/pc',
+          priceK: 'Nrs. 1450/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/karachi halwa.jpg`,
+          title: 'Karachi Halwa',
+          priceP: 'Nrs. 55/pc',
+          priceK: 'Nrs. 1750/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/anjeer chikki.jpg`,
+          title: 'Agneer Chakki',
+          priceP: 'Nrs. 70/pc',
+          priceK: 'Nrs. 3150/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/malai chop.jpg`,
+          title: 'Malai Chap',
+          priceP: 'Nrs. 100/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/malai roll.jpg`,
+          title: 'Malai Roll',
+          priceP: 'Nrs. 100/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/Balushahisix.jpg`,
+          title: 'Balushahi',
+          priceP: 'Nrs. 75/pc',
+          priceK: 'Nrs. 1700/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/ballushai.jpg`,
+          title: 'Balushahi/Khajuri',
+          priceP: 'Nrs. 75/pc',
+          priceK: 'Nrs. 1700/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/chenna roll.jpg`,
+          title: 'Chenna Roll',
+          priceP: 'Nrs. 95/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/boondi-laddu-small.jpg`,
+          title: 'Boondi Laddu (Small Box)',
+          priceP: '--',
+          priceK: 'Nrs. 750/box'
+        },
+        {
+          imgSrc: `${sweetPath}/BOONDI LADDU.jpg`,
+          title: 'Boondi Laddu',
+          priceP: 'Nrs. 55/pc',
+          priceK: 'Nrs. 1350/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/boondi laddu mota.jpg`,
+          title: 'Boondi Laddu-Mota',
+          priceP: 'Nrs. 55/pc',
+          priceK: 'Nrs. 1350/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/boondia.jpg`,
+          title: 'Boondia',
+          priceP: '--',
+          priceK: 'Nrs. 1600/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/milk cake.jpg`,
+          title: 'Milk Cake',
+          priceP: 'Nrs. 60/pc',
+          priceK: 'Nrs. 1750/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/Rasbari.jpg`,
+          title: 'Rasbari',
+          priceP: 'Nrs. 45/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/Soan-Papdi.jpg`,
+          title: 'Soan Papdi',
+          priceP: '--',
+          priceK: 'NRs. 430 for 400 Grams'
+        },
+        {
+          imgSrc: `${sweetPath}/besan-plain.png`,
+          title: 'Besan Special Laddu',
+          priceP: 'Nrs. 75/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/sohan halwa.jpg`,
+          title: 'SOHAN HALWA 200GM',
+          priceP: 'Nrs. 150/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/Kalakand.jpg`,
+          title: 'Kalakand',
+          priceP: 'Nrs. 50/pc',
+          priceK: 'Nrs. 1750/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/kala jamun.jpg`,
+          title: 'Kalajamun',
+          priceP: 'Nrs. 60/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/gulab.png`,
+          title: 'Small Gulab Jamun',
+          priceP: '--',
+          priceK: 'Nrs. 1200/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/Fruit-Cream.jpg`,
+          title: 'Fruit Cream',
+          priceP: 'Nrs. 110/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/cham cham.jpg`,
+          title: 'Cham Cham',
+          priceP: 'Nrs. 80/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/Rasmadhuri.jpg`,
+          title: 'Rasmadhuri',
+          priceP: 'Nrs. 120/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/rajbhog.jpg`,
+          title: 'Rajbhog',
+          priceP: 'Nrs. 90/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/rajasthani peda.jpg`,
+          title: 'Rajasthani Peda',
+          priceP: 'Nrs. 60/pc',
+          priceK: 'Nrs. 1750/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/mathura peda.png`,
+          title: 'Mathura Peda',
+          priceP: 'Nrs. 50/pc',
+          priceK: 'Nrs. 1750/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/khoya barfi.jpg`,
+          title: 'Khoya Barfi',
+          priceP: 'Nrs. 50/pc',
+          priceK: 'Nrs. 1750/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/AnjeerCake.webp`,
+          title: 'Anjeer Cake',
+          priceP: 'Nrs. 65/pc',
+          priceK: 'Nrs. 2450/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/kesar-ghevar-small.jpg`,
+          title: 'Kesar Ghewar (SMALL)',
+          priceP: 'Nrs. 130/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/kesar-ghevar-small.jpg`,
+          title: 'Kesar Ghewar (BIG)',
+          priceP: 'Nrs. 380/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/doodh-ghewar-small.jpg`,
+          title: 'Doodh Ghewar (SMALL)',
+          priceP: 'Nrs. 130/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/doodh-ghewar-small.jpg`,
+          title: 'Doodh Ghewar (BIG)',
+          priceP: 'Nrs. 380/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/malai-ghewar-small.jpg`,
+          title: 'Malai Ghewar (BIG)',
+          priceP: 'Nrs. 410/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/malai-ghewar-small.jpg`,
+          title: 'Malai Ghewar (SMALL)',
+          priceP: 'Nrs. 165/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/56-bhog.jpg`,
+          title: '56-Bhog-1pc',
+          priceP: 'Nrs. 3300/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/56-bhog.jpg`,
+          title: '56-Bhog-2pc',
+          priceP: 'Nrs. 6000/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/56-bhog.jpg`,
+          title: '56-Bhog-3pc',
+          priceP: 'Nrs. 9000/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/56-bhog.jpg`,
+          title: '56-Bhog-4pc',
+          priceP: 'Nrs. 11000/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/dahi-big.jpeg`,
+          title: 'Dahi (BIG)',
+          priceP: 'Nrs. 400/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/dahi-small.jpg`,
+          title: 'Dahi (SMALL)',
+          priceP: 'Nrs. 130/pc',
+          priceK: '--'
+        },
         ]
 }
