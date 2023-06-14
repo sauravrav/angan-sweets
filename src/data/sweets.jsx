5418554459a7285329fe6b6f6fc4be401b3b864f
sauravb <s.bhatta2383@gmail.com>
2023-06-14 23:50:27 +0545

    Added sweets up to kacha gola

--- src/data/sweets.jsx
+++ src/data/sweets.jsx
@@ -38,5 +38,77 @@ export const sweets = {
              priceP: 'Nrs. 75/pc',
              priceK: 'Nrs. 1700/kg'
         },
+        {
+             imgSrc: `${sweetPath}/khuwa-badam.jpg`,
+             title: 'Khuwa Badam',
+             priceP: 'Nrs. 50/pc',
+             priceK: 'Nrs. 1750/kg'
+        },
+        {
+             imgSrc: `${sweetPath}/besan-plain-laddu.png`,
+             title: 'Besan Plain Laddu',
+             priceP: 'Nrs. 70/pc',
+             priceK: 'Nrs. 1700/kg'
+        },
+        {
+             imgSrc: `${sweetPath}/dry-fruits-laddu.jpg`,
+             title: 'Dry Fruits Laddu',
+             priceP: '--',
+             priceK: 'Nrs. 2950/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/pinni.png`,
+          title: 'Pinni Laddu',
+          priceP: 'Nrs. 60/pc',
+          priceK: 'Nrs. 1450/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/anarkali.jpg`,
+          title: 'Anarkali',
+          priceP: 'Nrs. 90/pc',
+          priceK: '--'
+        },
+        {
+          imgSrc: `${sweetPath}/goondh-laddu.jpg`,
+          title: 'Gondh Laddu',
+          priceP: 'Nrs. 75/pc',
+          priceK: 'Nrs. 1800/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/goond-chakki.jpg`,
+          title: 'Gondh Chakki',
+          priceP: 'Nrs. 60/pc',
+          priceK: 'Nrs. 1450/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/sugar-free-laddu.png`,
+          title: 'Sugar Free Laddu',
+          priceP: 'Nrs. 75/pc',
+          priceK: 'Nrs. 2350/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/sugar-free-peda.jpg`,
+          title: 'Sugar Free Peda',
+          priceP: 'Nrs. 75/pc',
+          priceK: 'Nrs. 2350/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/pista-laugge.webp`,
+          title: 'Pista Laugge',
+          priceP: 'Nrs. 150/pc',
+          priceK: 'Nrs. 6500/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/gulkand-barfi.jpg`,
+          title: 'Gulkand Barfi',
+          priceP: 'Nrs. 50/pc',
+          priceK: 'Nrs. 1800/kg'
+        },
+        {
+          imgSrc: `${sweetPath}/kacha-gola.jpg`,
+          title: 'Kachha Gola',
+          priceP: 'Nrs. 75/pc',
+          priceK: '--'
+        },
         ]
 }
