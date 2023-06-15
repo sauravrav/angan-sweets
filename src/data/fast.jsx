a7b63da62623875cab460c4c0eeb199c4b344464
sauravb <s.bhatta2383@gmail.com>
2023-06-15 23:45:49 +0545

    Added fast food and north indian data

--- src/data/fast.jsx
+++ src/data/fast.jsx
@@ -5,37 +5,73 @@ export const fasts = {
             {
                 imgSrc: `${fastPath}/chola-bhatura.jpeg`,
                 title: 'Chola Bhatura',
-                priceP: '--',
+                priceP: '',
                 priceK: 'Nrs. 375'
            },
             {
                 imgSrc: `${fastPath}/grilled-sandwich.jpg`,
                 title: 'Grilled Sandwich',
-                priceP: '--',
+                priceP: '',
                 priceK: 'Nrs. 325'
            },
             {
                 imgSrc: `${fastPath}/paobhaji.jpeg`,
                 title: 'Paobhaji',
-                priceP: '--',
+                priceP: '',
                 priceK: 'Nrs. 330'
            },
             {
                 imgSrc: `${fastPath}/pasta-with-tomato-sauce.jpeg`,
                 title: 'Pasta With Tomato Sauce',
-                priceP: '--',
+                priceP: '',
                 priceK: 'Nrs. 450'
            },
             {
                 imgSrc: `${fastPath}/pasta-with-white-sauce.jpeg`,
                 title: 'Pasta With White Sauce',
-                priceP: '--',
+                priceP: '',
                 priceK: 'Nrs. 450'
            },
             {
                 imgSrc: `${fastPath}/cheese-pizza.jpeg`,
                 title: 'Cheese Pizza',
-                priceP: '--',
+                priceP: '',
                 priceK: 'Nrs. 525'
+           },
+            {
+                imgSrc: `${fastPath}/tomato-pizza.jpg`,
+                title: 'Tomato Pizza',
+                priceP: '',
+                priceK: 'Nrs. 575'
+           },
+            {
+                imgSrc: `${fastPath}/mushroom-pizza.jpg`,
+                title: 'Mushroom Pizza',
+                priceP: '',
+                priceK: 'Nrs. 595'
+           },
+            {
+                imgSrc: `${fastPath}/onion-capsicum-pizza.jpg`,
+                title: 'Onion Capsicum Pizza',
+                priceP: '',
+                priceK: 'Nrs. 595'
+           },
+            {
+                imgSrc: `${fastPath}/capsicum-tomato-pizza.jpeg`,
+                title: 'Capsicum Tomato Pizza',
+                priceP: '',
+                priceK: 'Nrs. 595'
+           },
+            {
+                imgSrc: `${fastPath}/angan-special-pizza.jpeg`,
+                title: 'Angan Special Pizza',
+                priceP: '',
+                priceK: 'Nrs. 675'
+           },
+            {
+                imgSrc: `${fastPath}/paneer-kulcha.jpg`,
+                title: 'Paneer Kulcha',
+                priceP: '',
+                priceK: 'Nrs. 150'
            },
         ]}
--- src/data/north.jsx
+++ src/data/north.jsx
@@ -5,37 +5,73 @@ export const northIndian = {
             {
                 imgSrc: `${northPath}/dal-makhani.jpeg`,
                 title: 'Dal Makhani',
-                priceP: '--',
+                priceP: '',
                 priceK: 'Nrs. 525'
            },
             {
                 imgSrc: `${northPath}/shahi-paneer.jpeg`,
                 title: 'Shahi Paneer',
-                priceP: '--',
+                priceP: '',
                 priceK: 'Nrs. 575'
            },
             {
                 imgSrc: `${northPath}/season-vegetable.jpeg`,
                 title: 'Seasonal Vegetable',
-                priceP: '--',
+                priceP: '',
                 priceK: 'Nrs. 525'
            },
             {
                 imgSrc: `${northPath}/paneer-butter-masala.webp`,
                 title: 'Paneer Butter Masala',
-                priceP: '--',
+                priceP: '',
                 priceK: 'Nrs. 575'
            },
             {
                 imgSrc: `${northPath}/chana-masala.jpeg`,
                 title: 'Chana Masala',
-                priceP: '--',
+                priceP: '',
                 priceK: 'Nrs. 450'
            },
             {
                 imgSrc: `${northPath}/rajma.jpeg`,
                 title: 'Rajma',
-                priceP: '--',
+                priceP: '',
                 priceK: 'Nrs. 450'
+           },
+            {
+                imgSrc: `${northPath}/dal-fry.jpg`,
+                title: 'Dal Fry',
+                priceP: '',
+                priceK: 'Nrs. 450'
+           },
+            {
+                imgSrc: `${northPath}/pulao.jpg`,
+                title: 'Pulao',
+                priceP: '',
+                priceK: 'Nrs. 425'
+           },
+            {
+                imgSrc: `${northPath}/plain-rice.jpeg`,
+                title: 'Plain Rice',
+                priceP: '',
+                priceK: 'Nrs. 325'
+           },
+            {
+                imgSrc: `${northPath}/raita.jpeg`,
+                title: 'Raita',
+                priceP: '',
+                priceK: 'Nrs. 355'
+           },
+            {
+                imgSrc: `${northPath}/green-salad.jpg`,
+                title: 'Green Salad',
+                priceP: '',
+                priceK: 'Nrs. 275'
+           },
+            {
+                imgSrc: `${northPath}/veg-biryani.jpeg`,
+                title: 'Veg Biryani',
+                priceP: '',
+                priceK: 'Nrs. 590'
            },
         ]}
